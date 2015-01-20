0387ca12a4783dd3503cf9f5ec03e8299b6e6c83
Dwylkz <xellosgeteternal@gmail.com>
2015-01-20 23:58:31 +0800

    init

---
+++ ss-utils/dlib/misc.scm
@@ -0,0 +1,10 @@
+(module
+  dlib-misc
+  (id)
+
+  (import scheme)
+
+  (define id
+    (lambda (x)
+      x))
+  )
---
+++ ss-utils/test.scm
@@ -0,0 +1,5 @@
+(include "dlib/misc.scm")
+
+(import (prefix dlib-misc d:))
+
+(write (d:id "1"))
